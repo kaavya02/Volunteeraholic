a6c7530be7420ebd53015dbdef6deb4cc2c759ab
Kaavya <kaavya0202@gmail.com>
2022-07-25 23:15:47 +0530

    view more done

--- lib/PostPage.dart
+++ lib/PostPage.dart
@@ -14,6 +14,51 @@ class _PostPageState extends State<PostPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(widget.post.eventName),),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset('assets/images/nus.jpg'),
+            Container(
+              padding: EdgeInsets.only(left: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 10,),
+                  Text('Organisation', style: TextStyle(fontSize: 15, fontWeight: FontWeight. bold),),
+                  SizedBox(height: 8,),
+                  Text(widget.post.orgName ,style: TextStyle(fontSize: 13.5,)),
+                  SizedBox(height: 10,),
+                  Text('Event Description', style: TextStyle(fontSize: 15, fontWeight: FontWeight. bold),),
+                  SizedBox(height: 8,),
+                  Text(widget.post.description ,style: TextStyle(fontSize: 13.5,)),
+                  SizedBox(height: 10,),
+                  Text('Voluteer Requirements', style: TextStyle(fontSize: 15, fontWeight: FontWeight. bold),),
+                  SizedBox(height: 8,),
+                  Text(widget.post.volunteerRequirements ,style: TextStyle(fontSize: 13.5,)),
+                  SizedBox(height: 10,),
+                  Text('Volunteer Rewards', style: TextStyle(fontSize: 15, fontWeight: FontWeight. bold),),
+                  SizedBox(height: 8,),
+                  Text(widget.post.volunteerRewards ,style: TextStyle(fontSize: 13.5,)),
+                  SizedBox(height: 10,),
+                  Text('Start Date', style: TextStyle(fontSize: 15, fontWeight: FontWeight. bold),),
+                  SizedBox(height: 8,),
+                  Text(widget.post.startDate.toString() ,style: TextStyle(fontSize: 13.5,)),
+                  SizedBox(height: 10,),
+                  Text('Commitment Period/ Duration', style: TextStyle(fontSize: 15, fontWeight: FontWeight. bold),),
+                  SizedBox(height: 8,),
+                  Text(widget.post.commitment ,style: TextStyle(fontSize: 13.5,)),
+                  SizedBox(height: 10,),
+                  Text('Sign Up link', style: TextStyle(fontSize: 15, fontWeight: FontWeight. bold),),
+                  SizedBox(height: 8,),
+                  Text(widget.post.linkToSignUp ,style: TextStyle(fontSize: 13.5,)),
+
+                ],
+              ),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
